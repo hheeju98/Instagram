6fafd74938d9434872abdf169badc44bd1252b90
hheeju98 <hheeju98@naver.com>
2022-11-12 22:56:05 +0900

    프로젝트 프론트엔드 clone

---
+++ src/main/java/site/metacoding/intagram_clone/IntagramCloneApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.intagram_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntagramCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntagramCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/intagram_clone/IntagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.intagram_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
